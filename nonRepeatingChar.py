a50a91c28c572f17fa960ce4e38effcf0af26247
annuvrat <annuvrat1098@gmail.com>
2026-06-03 16:11:02 +0530

    2 sum

---
+++ nonRepeatingChar.py
@@ -0,0 +1,22 @@
+s = "leetcode"
+
+
+def firstUniqChar(s):
+  counts = {}
+  result = []
+
+  for ch in s:
+    if ch not in counts:
+      counts[ch]= 0
+    counts[ch] += 1
+
+  for ch,i in counts.items():
+    if i==1:
+      result.append(ch)
+
+
+  return str(result)
+
+
+
+print(firstUniqChar(s))
---
+++ twoSum.py
@@ -0,0 +1,23 @@
+nums = [11,7,15,2]
+target = 9
+
+
+def twoSum(nums,target):
+    counts  ={}
+    for i,num in enumerate(nums):
+     needed = target-num
+     if needed in counts:
+        return [counts[needed],i]
+     
+     
+     counts[num]=i
+
+
+     
+
+
+    return False
+
+      
+print(twoSum(nums,target))
+ 
