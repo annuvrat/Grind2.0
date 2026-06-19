a5e005453d012fe8719867afeaa84eaca488cf39
annuvrat <annuvrat1098@gmail.com>
2026-06-19 10:47:38 +0530

    consecutives

---
+++ array/maxConsecutiveones.py
@@ -0,0 +1,28 @@
+nums = [1,1,1,1,0,1,1,0,1,1,1,1,1,1]
+
+
+def consecutives(nums):
+
+    count =0
+    max_count=0
+
+    for num in nums:
+        if num == 1:
+            count+=1
+            
+
+            if count>max_count:
+             max_count = count
+            # max_count=0
+         
+        else:
+            count =0
+            
+            
+    return max_count 
+
+
+
+
+
+print(consecutives(nums))
